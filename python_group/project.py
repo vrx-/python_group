cd37b85a0bf7c076e74fd263a38d51a9c757b912
vrx- <vrx@tamu.edu>
2014-11-14 10:31:14 -0600

    buoys

--- python_group/project.py
+++ python_group/project.py
@@ -1,6 +1,13 @@
-from netCDF4 import Dataset
+#!/usr/bin/env python
 
-PATH='/Users/vrx/Desktop/sst_xyt_dy.cdf'
+from netCDF4 import Dataset
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+
+PATH='sst_xyt_dy.cdf'
+
+
 data=Dataset(PATH)
 
 lat=data.variables['lat'][:]
@@ -11,3 +18,18 @@ time=data.variables['time'][:]
 T=data.variables['T_20'] #TEMPERATURE
 ST=data.variables['ST_6020'] #TEMPERATURE SOURCE
 QT=data.variables['QT_5020'] #TEMPERATURE QUALITY
+
+m = Basemap(projection='robin',
+            lat_0=0,
+            lon_0=-155,
+            resolution='c')
+
+fig = plt.figure(figsize=(10, 7))
+ax = fig.add_subplot(111)
+
+m.fillcontinents()
+
+x, y = m(*np.meshgrid(lon, lat))
+m.plot(x,y,'or')
+
+plt.show()
